630a5bb27a03e876e9413fb704a9ed0ea06e03d6
Mohammed Alzubaidy <mohalzubaidy@smail.hs-bremerhaven.de>
2025-03-09 04:15:24 +0100

    FEATURE: Skripte für Git Hooks hinzugefügt

--- Setup-Skript.sh
+++ Setup-Skript.sh
@@ -2,20 +2,20 @@
 echo "🔧 Installiere Git Hooks..."
 
 # Pre-Commit Hook
-cp ./hooks/pre-commit .git/hooks/pre-commit
+cp hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
 # Commit-Message Hook
-cp ./hooks/commit-msg .git/hooks/commit-msg
+cp hooks/commit-msg .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
 
 # Pre-Receive Hook (Serverseitig)
-scp ./hooks/pre-receive mohalzubaidy@hopper:/home/gitterbett/swe3-2024-12/hooks/pre-receive
-ssh mohalzubaidy@hopper "chmod +x /home/gitterbett/swe3-2024-12/hooks/pre-receive"
+#scp ./hooks/pre-receive mohalzubaidy@hopper:/home/gitterbett/swe3-2024-12/hooks/pre-receive
+#ssh mohalzubaidy@hopper "chmod +x /home/gitterbett/swe3-2024-12/hooks/pre-receive"
 
 # Post-Receive Hook (Serverseitig)
-scp ./hooks/post-receive mohalzubaidy@hopper:/home/gitterbett/swe3-2024-12/hooks/post-receive
-ssh mohalzubaidy@hopper "chmod +x /home/gitterbett/swe3-2024-12/hooks/post-receive"
+#scp ./hooks/post-receive mohalzubaidy@hopper:/home/gitterbett/swe3-2024-12/hooks/post-receive
+#ssh mohalzubaidy@hopper "chmod +x /home/gitterbett/swe3-2024-12/hooks/post-receive"
 
 echo "✅ Hooks erfolgreich installiert!"
 
