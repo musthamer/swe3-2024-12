99a7cc9698a750c8ee13b81df4242d857f7686f8
Mohammed Alzubaidy <mohalzubaidy@smail.hs-bremerhaven.de>
2025-03-12 02:22:28 +0100

    FEATURE: Fehlerhafter Test

--- Test.java
+++ Test.java
@@ -1,1 +1,6 @@
-import java.util.List; import java.util.ArrayList; class Test { public static void main(String[] args) { System.out.println("Test"); } }
+
+class Test {
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
