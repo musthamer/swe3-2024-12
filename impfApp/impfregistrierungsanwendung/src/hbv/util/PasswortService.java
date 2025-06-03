fc90596b524bd1eed159601877f00f15c5f530d6
Mustafa Thamer <musthamer@smail.hs-bremerhaven.de>
2025-06-03 15:37:41 +0200

    NEUE FUNKTION: Alles neu

--- impfApp/impfregistrierungsanwendung/src/hbv/util/PasswortService.java
+++
@@ -1,6 +0,0 @@
-package hbv.util;
-
-public interface PasswortService {
-    String hashePasswortMitSalt(String passwort);
-    boolean passwortVergleichen(String passwort, String gespeicherterHash);
-}
--- impfregistrierung/bin/clean-all.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rm -rf tmp/ local/* lib/* app/WEB-INF/lib/* build/* target/*
